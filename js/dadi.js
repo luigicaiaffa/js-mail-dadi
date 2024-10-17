1f0388ad864d47d255cfeaa26948149206b99155
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-10-17 15:41:21 +0200

    Dadi- add comments

---
+++ js/dadi.js
@@ -0,0 +1,18 @@
+// ! Gioco dei dadi
+// * Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// * Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// # Raccolta Dati
+// genera un numero random per il giocatore e assegnalo a una variabile
+// genera un numero random per il computer e assegnalo a una variabile
+// crea variabile messaggio di output
+
+// # Elaborazione
+// SE il numero del giocatore è maggiore del numero del computer
+// messaggio di output giocatore vincente
+
+// ALTRIMENTI
+// messaggio di output computer vincente
+
+// # Output
+// messaggio di output
--- js/mail.js
+++ js/mail.js
@@ -55,7 +55,3 @@ console.log(`
     isUserMailValid : ${isUserMailValid} 
     outputMessage : ${outputMessage} 
 `);
-
-// ! Gioco dei dadi
-// * Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-// * Stabilire il vincitore, in base a chi fa il punteggio più alto.
