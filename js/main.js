27c53153757e412d0745bc7bfa1a34693e98f866
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-10-17 14:56:16 +0200

    add comments

--- js/main.js
+++ js/main.js
@@ -3,6 +3,23 @@
 // * controlla che sia nella lista di chi può accedere,
 // * stampa un messaggio appropriato sull'esito del controllo.
 
+// # Raccolta Dati
+// lista delle mail registrate
+// chiedi una mail all'utente con un prompt e assegnale una variabile
+// crea variabile per messaggio di output
+
+// # Elaborazione
+// verifica se la mail è inserita in formato valido
+
+// SE la mail inserita è valida AND uguale ad una mail della lista di mail
+//      messaggio di output esito positivo
+
+// ALTRIMENTI
+//      messaggio di output esito negativo
+
+// # Output
+// messaggio di output
+
 // ! Gioco dei dadi
 // * Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // * Stabilire il vincitore, in base a chi fa il punteggio più alto.
